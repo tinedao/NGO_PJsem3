46f861c1227f0eda17d9ad011d738900205c4a18
Tine Đào <122794534+tinedao@users.noreply.github.com>
2023-06-17 00:27:33 +0700

    update usercontroller

--- Areas/Customer/Controllers/UserController.cs
+++ Areas/Customer/Controllers/UserController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using NGO_PJsem3.Models;
@@ -14,11 +15,79 @@ namespace NGO_PJsem3.Areas.Customer.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
-        [HttpPost]
-        [Route("CreateUser")]
+        private readonly HttpClient _httpClient;
+        private readonly IPasswordHasher<Users> _passwordHasher;
+
+        public UserController(IPasswordHasher<Users> passwordHasher)
+        {
+            _passwordHasher = passwordHasher;
+        }
+
+        public UserController(IHttpClientFactory httpClientFactory, IPasswordHasher<Users> passwordHasher)
+        {
+            _httpClient = httpClientFactory.CreateClient();
+            _passwordHasher = passwordHasher;
+        }
+
+        [HttpGet("checkExistence")]
+        public async Task<IActionResult> CheckExistence(string email, string phoneNumber)
+        {
+            // Kiểm tra sự tồn tại của email và số điện thoại
+            bool isEmailExists = await CheckEmailExistence(email);
+            bool isPhoneNumberExists = await CheckPhoneNumberExistence(phoneNumber);
+
+            return Ok(new { EmailExists = isEmailExists, PhoneNumberExists = isPhoneNumberExists });
+        }
+
+        private async Task<bool> CheckEmailExistence(string email)
+        {
+            // Gửi yêu cầu kiểm tra sự tồn tại của email đến API
+            var response = await _httpClient.GetAsync($"https://localhost:7296/NGO/Validation/CheckEmailExistence?email={email}");
+
+            if (!response.IsSuccessStatusCode)
+            {
+                // Xử lý lỗi khi kiểm tra email tồn tại
+                return false;
+            }
+
+            var result = await response.Content.ReadAsStringAsync();
+            return bool.Parse(result);
+        }
+
+        private async Task<bool> CheckPhoneNumberExistence(string phoneNumber)
+        {
+            // Gửi yêu cầu kiểm tra sự tồn tại của số điện thoại đến API
+            var response = await _httpClient.GetAsync($"https://localhost:7296/NGO/Validation/CheckPhoneNumberExistence?phoneNumber={phoneNumber}");
+
+            if (!response.IsSuccessStatusCode)
+            {
+                // Xử lý lỗi khi kiểm tra số điện thoại tồn tại
+                return false;
+            }
+
+            var result = await response.Content.ReadAsStringAsync();
+            return bool.Parse(result);
+        }
+
+        [HttpPost("CreateUser")]
         public async Task<ActionResult> CreateUser(string username, string password, string imgUser, string email, string fullName, string address, string phoneNumber)
         {
-            // Tạo đối tượng User với thông tin người dùng
+            // Kiểm tra validate trước khi thêm người dùng mới
+            bool isEmailExists = await CheckEmailExistence(email);
+            bool isPhoneNumberExists = await CheckPhoneNumberExistence(phoneNumber);
+
+            // Kiểm tra xem email và số điện thoại đã tồn tại hay chưa
+            if (isEmailExists)
+            {
+                return BadRequest("Email already exists");
+            }
+
+            if (isPhoneNumberExists)
+            {
+                return BadRequest("Phone number already exists");
+            }
+
+            // Tiếp tục xử lý thêm người dùng mới
             var user = new Users
             {
                 Username = username,
@@ -30,53 +99,28 @@ namespace NGO_PJsem3.Areas.Customer.Controllers
                 PhoneNumber = phoneNumber
             };
 
-            // Chuyển đối tượng User thành JSON
+            // Gửi yêu cầu thêm người dùng mới đến API
             var jsonUser = JsonConvert.SerializeObject(user);
+            var httpContent = new StringContent(jsonUser, Encoding.UTF8, "application/json");
+            var response = await _httpClient.PostAsync("https://localhost:7296/NGO/User/CreateUser", httpContent);
 
-            using (var httpClient = new HttpClient())
+            if (response.IsSuccessStatusCode)
             {
-                // Tạo nội dung yêu cầu POST với JSON người dùng
-                var httpContent = new StringContent(jsonUser, Encoding.UTF8, "application/json");
-
-                // Gửi yêu cầu POST tới API
-                var response = await httpClient.PostAsync("https://localhost:7296/NGO/User/CreateUser", httpContent);
-
-                // Kiểm tra phản hồi từ API
-                if (response.IsSuccessStatusCode)
-                {
-                    // Người dùng đã được tạo thành công
-                    return Ok("User created successfully");
-                }
-                else
-                {
-                    // Có lỗi xảy ra trong quá trình tạo người dùng
-                    var errorMessage = await response.Content.ReadAsStringAsync();
-                    return BadRequest(errorMessage);
-                }
+                return Ok("User created successfully");
+            }
+            else
+            {
+                var errorMessage = await response.Content.ReadAsStringAsync();
+                return BadRequest(errorMessage);
             }
         }
-
 
         private string HashPassword(string password)
         {
-            using (var sha256 = SHA256.Create())
-            {
-                // Chuyển mật khẩu thành mảng byte
-                byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
-
-                // Tính toán giá trị băm của mật khẩu
-                byte[] hashBytes = sha256.ComputeHash(passwordBytes);
-
-                // Chuyển giá trị băm thành chuỗi hexa
-                StringBuilder builder = new StringBuilder();
-                for (int i = 0; i < hashBytes.Length; i++)
-                {
-                    builder.Append(hashBytes[i].ToString("x2"));
-                }
-
-                // Trả về chuỗi hexa làm mật khẩu đã mã hóa
-                return builder.ToString();
-            }
+            // Mã hóa mật khẩu sử dụng bcrypt
+            var passwordHash = _passwordHasher.HashPassword(null, password);
+            return passwordHash;
         }
     }
+
 }
